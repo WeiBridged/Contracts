6c442b2fdab339cb67437d8d223be5c2b003ece9
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2022-09-08 18:00:45 -0400

    Update bridgeETHtoETH.sol

--- contracts/bridgeETHtoETH.sol
+++ contracts/bridgeETHtoETH.sol
@@ -1,12 +1,14 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.16;
+pragma solidity 0.8.17;
 
 contract GoerliBridge {
 
     mapping(address => uint) public optimismLockedETH;
 
+    error msgValueZero(); //Using custom errors with revert saves gas compared to using require. 
+
     function lockTokensForOptimism() public payable {
-        require(msg.value > 0 ,"MSG.VALUE_MUST_BE_GREATER_THAN_0.");
+        if (msg.value == 0) { revert msgValueZero(); } 
         optimismLockedETH[msg.sender] += msg.value;
     }
 
@@ -18,21 +20,25 @@ contract OptimismBridge {
 
     mapping(address => uint) public optimismBridgedETH;
 
+    error msgValueZero(); //Using custom errors with revert saves gas compared to using require. 
+    error notOwnerAddress();
+    error bridgedAlready();
+
     constructor() {
         Owner = msg.sender;
     }
 
     function addBridgeLiqudity() public payable {
-        require(msg.sender == Owner, "ONLY_OWNER_ADDRESS_CAN_USE_THIS_FUNCTION!");
-        require(msg.value > 0 ,"MSG.VALUE_MUST_BE_GREATER_THAN_0.");
+        if (msg.sender != Owner) { revert notOwnerAddress(); } 
+        if (msg.value == 0) { revert msgValueZero(); } 
     }
 
-    function payoutOptimismETH(uint oracleBridgeLockedGoerliETH) public {
-        require(msg.sender == Owner, "ONLY_OWNER_ADDRESS_CAN_USE_THIS_FUNCTION!");
-        require(oracleBridgeLockedGoerliETH > optimismBridgedETH[msg.sender],"MSG.VALUE_MUST_BE_GREATER_THAN_0.");
-        uint sendETH = oracleBridgeLockedGoerliETH- optimismBridgedETH[msg.sender];
-        optimismBridgedETH[msg.sender] += sendETH;
-        payable(msg.sender).transfer(sendETH);
+    function payoutOptimismETH(address bridgeUser, uint oracleBridgeLockedGoerliETH) public {
+        if (msg.sender != Owner) { revert notOwnerAddress(); } 
+        if (oracleBridgeLockedGoerliETH <= optimismBridgedETH[bridgeUser]) { revert bridgedAlready(); } 
+        uint sendETH = oracleBridgeLockedGoerliETH - optimismBridgedETH[bridgeUser];
+        optimismBridgedETH[bridgeUser] += sendETH;
+        payable(bridgeUser).transfer(sendETH);
     }
 
 }
