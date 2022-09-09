9ace82685c7ed69d0aa3cbfea69348e272e0c4be
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2022-09-08 20:09:21 -0400

    Update bridgeETHtoETH.sol

--- contracts/bridgeETHtoETH.sol
+++ contracts/bridgeETHtoETH.sol
@@ -3,102 +3,42 @@ pragma solidity 0.8.17;
 
 contract GoerliBridge {
 
-    address public immutable Owner;    
-
-    mapping(address => uint) public lockedForOptimismETH;
-    mapping(address => uint) public goerliBridgedETH;
+    mapping(address => uint) public optimismLockedETH;
 
     error msgValueZero(); //Using custom errors with revert saves gas compared to using require. 
-    error msgValueLessThan1000(); 
-    error msgValueDoesNotCoverFee(); 
-    error notOwnerAddress();
-    error bridgedAlready();
-    error bridgeEmpty();
 
-    constructor() {
-        Owner = msg.sender;
-    }
-
-    MockOptimismBridge public optimismBridgeInstance;    
-
-    function lockTokensForOptimism(uint bridgeAmount) public payable {
-        if (bridgeAmount < 1000) { revert msgValueLessThan1000(); } 
-        if (msg.value != (1003*bridgeAmount)/1000 ) { revert msgValueDoesNotCoverFee(); } 
-        lockedForOptimismETH[msg.sender] += (1000*msg.value)/1003;
-        payable(Owner).transfer(msg.value);
-    }
-
-    function unlockedOptimismETH() public {
-        if (optimismBridgeInstance.lockedForGoerliETH(msg.sender) <= goerliBridgedETH[msg.sender]) { revert bridgedAlready(); } 
-        uint sendETH = optimismBridgeInstance.lockedForGoerliETH(msg.sender)- goerliBridgedETH[msg.sender];
-        goerliBridgedETH[msg.sender] += sendETH;
-        payable(msg.sender).transfer(sendETH);
-    }
-
-    function ownerAddBridgeLiqudity() public payable {
-        if (msg.sender != Owner) { revert notOwnerAddress(); } 
+    function lockTokensForOptimism() public payable {
         if (msg.value == 0) { revert msgValueZero(); } 
-    }
-
-    function ownerRemoveBridgeLiqudity() public  {
-        if (address(this).balance == 0) { revert bridgeEmpty(); } 
-        payable(Owner).transfer(address(this).balance);
-    }
-
-    function mockOwnerOptimismBridgeAddress(address _token) public{
-        if (msg.sender != Owner) { revert notOwnerAddress(); } 
-        optimismBridgeInstance = MockOptimismBridge(_token); //ERC20 token address goes here.;
+        optimismLockedETH[msg.sender] += msg.value;
     }
 
 }
 
-contract MockOptimismBridge {
+contract OptimismBridge {
 
     address public immutable Owner;    
 
-    mapping(address => uint) public lockedForGoerliETH;
     mapping(address => uint) public optimismBridgedETH;
-    
+
     error msgValueZero(); //Using custom errors with revert saves gas compared to using require. 
-    error msgValueLessThan1000(); 
-    error msgValueDoesNotCoverFee(); 
     error notOwnerAddress();
     error bridgedAlready();
-    error bridgeEmpty();
-
-    GoerliBridge public goerliBridgeInstance;
 
     constructor() {
         Owner = msg.sender;
     }
 
-    function lockTokensForGoerli(uint bridgeAmount) public payable {
-        if (bridgeAmount < 1000) { revert msgValueLessThan1000(); } 
-        if (msg.value != (1003*bridgeAmount)/1000 ) { revert msgValueDoesNotCoverFee(); } 
-        lockedForGoerliETH[msg.sender] += (1000*msg.value)/1003;
-        payable(Owner).transfer(msg.value);
-    }
-
-    function unlockedOptimismETH() public {
-        if (goerliBridgeInstance.lockedForOptimismETH(msg.sender) <= optimismBridgedETH[msg.sender]) { revert bridgedAlready(); } 
-        uint sendETH = goerliBridgeInstance.lockedForOptimismETH(msg.sender)- optimismBridgedETH[msg.sender];
-        optimismBridgedETH[msg.sender] += sendETH;
-        payable(msg.sender).transfer(sendETH);
-    }
-
-    function ownerAddBridgeLiqudity() public payable {
+    function addBridgeLiqudity() public payable {
         if (msg.sender != Owner) { revert notOwnerAddress(); } 
         if (msg.value == 0) { revert msgValueZero(); } 
     }
 
-    function ownerRemoveBridgeLiqudity() public  {
-        if (address(this).balance == 0) { revert bridgeEmpty(); } 
-        payable(Owner).transfer(address(this).balance);
-    }
-
-    function mockOwnerOptimismBridgeAddress(address _token) public{
+    function payoutOptimismETH(address bridgeUser, uint oracleBridgeLockedGoerliETH) public {
         if (msg.sender != Owner) { revert notOwnerAddress(); } 
-        goerliBridgeInstance = GoerliBridge(_token); //ERC20 token address goes here.;
+        if (oracleBridgeLockedGoerliETH <= optimismBridgedETH[bridgeUser]) { revert bridgedAlready(); } 
+        uint sendETH = oracleBridgeLockedGoerliETH - optimismBridgedETH[bridgeUser];
+        optimismBridgedETH[bridgeUser] += sendETH;
+        payable(bridgeUser).transfer(sendETH);
     }
 
 }
