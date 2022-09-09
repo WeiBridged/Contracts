441e041401f0110868bfb1bb7daadf0dbc6dce4b
MarcusWentz <markzcollege404@gmail.com>
2022-09-09 13:31:53 -0400

    Updated mock bridge address function name and Hardhat tested

---
+++ test/unit/test/bridgeTest.js
@@ -0,0 +1,106 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Faucet Tests:", function () {
+
+      let MockGoerliBridge;
+      let MockGoerliBridgeDeployed;
+      let MockOptimismBridge;
+      let MockOptimismBridgeDeployed;
+      let owner;
+      let addr1;
+      let addr2;
+      let addrs;
+
+      beforeEach(async function () {
+        MockGoerliBridge = await ethers.getContractFactory("MockGoerliBridge");
+        [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
+        MockGoerliBridgeDeployed = await MockGoerliBridge.deploy();
+        MockOptimismBridge = await ethers.getContractFactory("MockOptimismBridge");
+        [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
+        MockOptimismBridgeDeployed = await MockOptimismBridge.deploy();
+      });
+
+      describe("constructor()", function () {
+          it("relayAddress set", async function () {
+            expect(await MockGoerliBridgeDeployed.Owner()).to.equal(owner.address);
+          });
+          it("relayAddress set", async function () {
+            expect(await MockOptimismBridgeDeployed.Owner()).to.equal(owner.address);
+          });
+       });
+
+       describe("mockOwnerOptimismBridgeAddress(address _token)", function () {
+          it("Revert if relayAddress is not msg.sender", async function () {
+            await expect(
+              MockGoerliBridgeDeployed.connect(addr1).mockOwnerOptimismBridgeAddress(MockOptimismBridgeDeployed.address)
+            ).to.be.revertedWith("notOwnerAddress()");
+          });
+
+        });
+
+        describe("mockOwnerGoerliBridgeAddress(address _token)", function () {
+           it("Revert if relayAddress is not msg.sender", async function () {
+             await expect(
+               MockOptimismBridgeDeployed.connect(addr1).mockOwnerGoerliBridgeAddress(MockGoerliBridgeDeployed.address)
+             ).to.be.revertedWith("notOwnerAddress()");
+           });
+
+         });
+
+       // describe("withdrawDirect()", function () {
+       //     it("Revert if no LINK in faucet", async function () {
+       //       await expect(
+       //         ContractDeployed.withdrawDirect()
+       //       ).to.be.revertedWith("Faucet does not have any more LINK (has less than 20 LINK currently).");
+       //     });
+       //     it("User gets 20 LINK, then gets reverted if asks again if 12 hours have not passed.", async function () {
+       //
+       //       const transactionCallAPI = await ERC20Deployed.transfer(ContractDeployed.address, "40000000000000000000");
+       //       const tx_receiptCallAPI = await transactionCallAPI.wait();
+       //       expect(await ERC20Deployed.balanceOf(ContractDeployed.address)).to.equal("40000000000000000000");
+       //
+       //       const transactionCallAPI2 = await ContractDeployed.connect(addr1).withdrawDirect();
+       //       const tx_receiptCallAPI2 = await transactionCallAPI2.wait();
+       //       expect(await ERC20Deployed.balanceOf(ContractDeployed.address)).to.equal("20000000000000000000");
+       //       expect(await ERC20Deployed.balanceOf(addr1.address)).to.equal("20000000000000000000");
+       //
+       //       await expect(
+       //         ContractDeployed.connect(addr1).withdrawDirect()
+       //       ).to.be.revertedWith("Current user must wait 12 hours for faucet cooldown.");
+       //
+       //     });
+       //
+       //  });
+       //
+       //  describe("withdrawRelay(relayCaller)", function () {
+       //      it("Revert if relayAddress is not msg.sender", async function () {
+       //        await expect(
+       //          ContractDeployed.connect(addr1).withdrawRelay(addr1.address)
+       //        ).to.be.revertedWith("Only the relay address can access this function.");
+       //      });
+       //      it("Revert if no LINK in faucet", async function () {
+       //        await expect(
+       //          ContractDeployed.withdrawRelay(addr1.address)
+       //        ).to.be.revertedWith("Faucet does not have any more LINK (has less than 20 LINK currently).");
+       //      });
+       //      it("User gets 20 LINK, then gets reverted if asks again if 12 hours have not passed.", async function () {
+       //
+       //        const transactionCallAPI = await ERC20Deployed.transfer(ContractDeployed.address, "40000000000000000000");
+       //        const tx_receiptCallAPI = await transactionCallAPI.wait();
+       //        expect(await ERC20Deployed.balanceOf(ContractDeployed.address)).to.equal("40000000000000000000");
+       //
+       //        const transactionCallAPI2 = await ContractDeployed.withdrawRelay(addr1.address);
+       //        const tx_receiptCallAPI2 = await transactionCallAPI2.wait();
+       //        expect(await ERC20Deployed.balanceOf(ContractDeployed.address)).to.equal("20000000000000000000");
+       //        expect(await ERC20Deployed.balanceOf(addr1.address)).to.equal("20000000000000000000");
+       //
+       //        await expect(
+       //          ContractDeployed.withdrawRelay(addr1.address)
+       //        ).to.be.revertedWith("Current user must wait 12 hours for faucet cooldown.");
+       //
+       //      });
+       //
+       //   });
+
+});
